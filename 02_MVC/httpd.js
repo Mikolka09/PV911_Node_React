b834c647cd09099cdc712d462237fd408a065796
Микола Матіос <mikolka09@gmail.com>
2021-09-13 17:49:04 +0300

    Express

--- 02_MVC/httpd.js
+++ 02_MVC/httpd.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+//Установить каталог для статических файлов
+app.use(express.static("public"));
+
 const controller = require("./controllers/simpleController")
 
 app.route("/1")
