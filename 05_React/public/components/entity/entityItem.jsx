e23eb30b7d815611ed834435a985845ffd4c7afd
Микола Матіос <mikolka09@gmail.com>
2021-09-27 17:54:42 +0300

    CRUD

--- 05_React/public/components/entity/entityItem.jsx
+++ 05_React/public/components/entity/entityItem.jsx
@@ -10,54 +10,98 @@ class EntityItem extends React.Component {
         };
     }
 
-    onChange (element){
-        this.state[element.target.name] = element.target.value;
+    onChange(element) {
+        const item = this.state.item;
+        item[element.target.name] = element.target.value;
+        this.setState({item: item});
     }
 
     componentDidMount() {
     }
 
-    render(){
-        if(this.state.error) return this.renderError(); // Если ошибка - вывожу ее
+    render() {
+        if (this.state.error) return this.renderError(); // Если ошибка - вывожу ее
         // if(!this.state.isLoaded) return this.renderLoading(); // Загружаюсь
-        if(this.state.item == null) return this.renderNewElement();
+        if (this.state.isEdit) return this.renderForm();
+        if (this.state.item == null) return this.renderNewElement();
         return this.renderData();
     }
 
-    renderForm(){
-        <div className="card col-3" key="CreateNewElement">
-            <div className="card-body">
-                Создать
-            </div>
-        </div>
+    cancelEditForm() {
+        this.setState({
+            isEdit: false,
+            item: JSON.parse(this.oldItem)
+        });
+        this.oldItem = null;
     }
 
-    // Предложить созать форму
-    renderNewElement(){
+    saveEditForm() {
+        if (this.props.create)
+        {
+            this.props.create(this.state.item);
+            this.state.item = null;
+        }
+        else
+            this.props.update(this.state.item);
+        this.setState({
+            isEdit: false
+        });
+        delete this.oldItem;
+    }
+
+    renderForm() {
         return (
             <div className="card col-3" key="CreateNewElement">
                 <div className="card-body">
+                    <input name="_id" type="hidden" value={this.state.item._id}/>
+                    <input name="name" type="text" value={this.state.item.name} onChange={this.onChange.bind(this)}/>
+                    <input type="button" value="Cancel" onClick={this.cancelEditForm.bind(this)}/>
+                    <input type="button" value="Save" onClick={this.saveEditForm.bind(this)}/>
+                </div>
+            </div>
+        );
+    }
+
+    openEditForm() {
+        this.oldItem = JSON.stringify(this.state.item);//Сохраняю старый элемент (null)
+        if (this.state.item == null) {
+            this.state.item = new Object();
+            this.state.item.name = "";
+        }//Если у меня создание нового элемента - то делаю чистый
+        this.setState({isEdit: true});
+    }
+
+    // Предложить созать форму
+    renderNewElement() {
+        return (
+            <div className="card col-3" key="CreateNewElement">
+                <div className="card-body" onClick={this.openEditForm.bind(this)}>
                     Создать
                 </div>
             </div>
         );
     }
 
+    delete(){
+        this.props.delete(this.props.item);
+    }
+
     // Вывод основного состояния компонента
-    renderData(){
+    renderData() {
         return (
             <div className="card col-3" key={this.state.item._id}>
                 <div className="card-body">
                     {this.state.item.name}
+                    <input type="button" value="edit" onClick={this.openEditForm.bind(this)}/>
+                    <input type="button" value="delete" onClick={this.delete.bind(this)}/>
                 </div>
             </div>
         );
     }
 
 
-
     // Компонент в состоянии загрузки
-    renderLoading(){
+    renderLoading() {
         return (
             <div className="d-flex justify-content-center">
                 <div className="spinner-border" role="status">
@@ -68,7 +112,7 @@ class EntityItem extends React.Component {
     }
 
     // Отображение компонента в состоянии ошибки
-    renderError(){
+    renderError() {
         return (
             <div className="alert alert-danger" role="alert">
                 Error: {this.state.error.message}
--- 05_React/public/components/entity/entityList.jsx
+++ 05_React/public/components/entity/entityList.jsx
@@ -9,11 +9,37 @@ class EntityList extends React.Component {
         };
     }
 
-    onChange (element){
+    onChange(element) {
         this.state[element.target.name] = element.target.value;
     }
 
     componentDidMount() {
+        this.Read();
+    }
+
+    Create(item) {
+        console.log(item);
+        fetch("/api/entities",
+            {
+                method: "POST",
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(item)
+            })
+            .then(response => response.json())
+            .then(item => {
+                const items = this.state.items;
+                items.push(item);
+                this.setState({
+                    isLoaded: true,
+                    items: items
+                });
+            })
+            .catch(err => {
+                this.setState({error: err})
+            });
+    }
+
+    Read() {
         console.log("Start get data:");
         fetch("/api/entities")
             .then(response => response.json())
@@ -25,33 +51,81 @@ class EntityList extends React.Component {
                     items: data
                 });
             })
-            .catch(err=> {this.setState ({error: err})});
+            .catch(err => {
+                this.setState({error: err})
+            });
     }
 
-    render(){
-        if(this.state.error) return this.renderError(); // Если ошибка - вывожу ее
-        if(!this.state.isLoaded) return this.renderLoading(); // Загружаюсь
+    Update(item) {
+        fetch("/api/entities",
+            {
+                method: "PUT",
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(item)
+            })
+            .then(response => response.json())
+            .then(item => {
+                const items = this.state.items;
+                items[items.indexOf(el => el._id == item._id)] = item;
+                this.setState({
+                    isLoaded: true,
+                    items: items
+                });
+            })
+            .catch(err => {
+                this.setState({error: err})
+            });
+    }
+
+    Delete(item) {
+        const items = this.state.items;
+        console.log("Delete: ");
+        console.log(item);
+        items.splice(items.indexOf(el => el._id == item._id), 1);
+        this.setState({
+            isLoaded: true,
+            items: items
+        });
+        fetch("/api/entities",
+            {
+                method: "DELETE",
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(item)
+            })
+            .then(response => {})
+            .then(item => {})
+            .catch(err => {
+                this.setState({error: err})
+            });
+
+    }
+
+
+    render() {
+        if (this.state.error) return this.renderError(); // Если ошибка - вывожу ее
+        if (!this.state.isLoaded) return this.renderLoading(); // Загружаюсь
         return this.renderData();
     }
 
     // Вывод основного состояния компонента
-    renderData(){
+    renderData() {
         return (
             <div className="container">
                 <div className="row">
                     {
-                        this.state.items.map( entity =>
-                            <EntityItem item={entity}></EntityItem>
+                        this.state.items.map(entity =>
+                            <EntityItem update={this.Update.bind(this)} delete={this.Delete.bind(this)} key={entity._id}
+                                        item={entity}></EntityItem>
                         )
                     }
-                    <EntityItem key={"newElement"} item={null}></EntityItem>
+                    <EntityItem create={this.Create.bind(this)} key={"newElement"} item={null}></EntityItem>
                 </div>
             </div>
         );
     }
 
     // Компонент в состоянии загрузки
-    renderLoading(){
+    renderLoading() {
         return (
             <div className="d-flex justify-content-center">
                 <div className="spinner-border" role="status">
@@ -62,7 +136,7 @@ class EntityList extends React.Component {
     }
 
     // Отображение компонента в состоянии ошибки
-    renderError(){
+    renderError() {
         return (
             <div className="alert alert-danger" role="alert">
                 Error: {this.state.error.message}
