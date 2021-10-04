34674c5c8feb4844fd9790bb2e7f3692cbe78cb2
Микола Матіос <mikolka09@gmail.com>
2021-10-04 16:43:11 +0300

    04.10.2021

---
+++ 07_ReactWebPack/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NovaPoshta from "./componets/NovaPoshta";
+
+function App() {
+  return (
+    <div className="App">
+      <NovaPoshta></NovaPoshta>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 07_ReactWebPack/src/componets/NovaPoshta.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class NovaPoshta extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Новая почта ТУТ Будет!</h1>
+            </div>
+        );
+    }
+
+}
